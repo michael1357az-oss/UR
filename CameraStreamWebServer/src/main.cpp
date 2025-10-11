3684661b25a6e66ad17f5a53c513721f8e877d8f
StrikeAtZeroHour <taklok1114@gmail.com>
2025-10-11 21:30:04 +0800

    reduce useless code

--- CameraStreamWebServer/src/main.cpp
+++ CameraStreamWebServer/src/main.cpp
@@ -9,21 +9,7 @@ int onboardled = 33;
 const char *ssid = "KiraIphone";   // WiFi SSID (network name)
 const char *password = "kira1114"; // WiFi password
 
-// Motor Pins
-const int PIN_IN1 = 12;
-const int PIN_IN2 = 13;
-const int PIN_IN3 = 15;
-const int PIN_IN4 = 16;
-// PWM pins
-const int PIN_ENA = 2;
-const int PIN_ENB = 14;
 
-// PWM settings
-const int FREQ = 5000;
-const int RES = 8;
-const uint8_t MOTOR_CHA_A = 2; // Avoid conflict with camera LEDC_CHANNEL_0
-const uint8_t MOTOR_CHA_B = 3;
-uint8_t currentSpeedLeft = 0, currentSpeedRight = 0;
 // ==================
 // Camera Pin Settings for AI-Thinker Module
 // ==================
@@ -94,65 +80,7 @@ void handle_jpg_stream(void)
     }
   }
 }
-void handle_action() {
-  Serial.println("action request detected");
-  if (server.hasArg("value")) {
-    int dir = server.arg("value").toInt();
-    motor_direction(dir);
-  }
-  server.sendHeader("Location", "/", true);
-  server.send(302, "text/plain", "");
-}
 
-void motor_direction(int dir) {
-  dir = constrain(dir, 0, 4);
-  switch (dir) {
-    case 0:
-      Serial.println("Motor stop");
-      digitalWrite(PIN_IN1, LOW);
-      digitalWrite(PIN_IN2, LOW);
-      digitalWrite(PIN_IN3, LOW);
-      digitalWrite(PIN_IN4, LOW);
-      currentSpeedLeft = 0;
-      currentSpeedRight = 0;
-      break;
-    case 1:
-      Serial.println("Motor move forward");
-      digitalWrite(PIN_IN1, HIGH);
-      digitalWrite(PIN_IN2, LOW);
-      digitalWrite(PIN_IN3, HIGH);
-      digitalWrite(PIN_IN4, LOW);
-      break;
-    case 2:
-      Serial.println("Motor move left");
-      digitalWrite(PIN_IN1, HIGH);
-      digitalWrite(PIN_IN2, LOW);
-      digitalWrite(PIN_IN3, LOW);
-      digitalWrite(PIN_IN4, LOW);
-      break;
-    case 3:
-      Serial.println("Motor move backward");
-      digitalWrite(PIN_IN1, LOW);
-      digitalWrite(PIN_IN2, HIGH);
-      digitalWrite(PIN_IN3, LOW);
-      digitalWrite(PIN_IN4, HIGH);
-      break;
-    case 4:
-      Serial.println("Motor move right");
-      digitalWrite(PIN_IN1, LOW);
-      digitalWrite(PIN_IN2, LOW);
-      digitalWrite(PIN_IN3, LOW);
-      digitalWrite(PIN_IN4, HIGH);
-      break;
-  }
-  if (dir == 0) {
-    ledcWrite(MOTOR_CHA_A, 0);
-    ledcWrite(MOTOR_CHA_B, 0);
-  } else {
-    ledcWrite(MOTOR_CHA_A, currentSpeedLeft);
-    ledcWrite(MOTOR_CHA_B, currentSpeedRight);
-  }
-}
 
 // Main page handler
 void handle_root()
@@ -178,7 +106,6 @@ void startCameraServer()
 {
   server.on("/", HTTP_GET, handle_root);
   server.on("/stream", HTTP_GET, handle_jpg_stream);
-  server.on("/action", HTTP_GET, handle_action);
   server.begin();
 }
 
@@ -222,8 +149,8 @@ void setup()
   // To adjust JPEG quality: 0 (best) to 63 (worst), lower value = higher quality
 
   config.frame_size = FRAMESIZE_QVGA; // Resolution: 1024x768. Change to FRAMESIZE_VGA (640x480) or others as needed.
-  config.jpeg_quality = 15;           // JPEG quality. Lower = better image, but slower FPS.
-  config.fb_count = 2;
+  config.jpeg_quality = 12;           // JPEG quality. Lower = better image, but slower FPS.
+  config.fb_count = 5;
 
   // ===============================================
 
