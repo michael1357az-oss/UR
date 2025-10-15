7b9674d63055af28bc299c7458473d518e05d202
Noir <107355979+NoitNoitE@users.noreply.github.com>
2025-10-15 21:38:37 +0800

    pin setting,wifi setting,turning logic

--- motor/src/main_eventlistener.cpp
+++ motor/src/main_eventlistener.cpp
@@ -11,15 +11,13 @@
 
 // Motor Pin
 #if L_MOTOR
-const int PIN_IN1 = 12; 
-const int PIN_IN2 = 13;
-const int PIN_ENA = 2;
+const int PIN_IN1 = 14; 
+const int PIN_IN2 = 15;
 #endif
 
 #if R_MOTOR 
-const int PIN_IN3 = 14;
-const int PIN_IN4 = 0;
-const int PIN_ENB = 4; 
+const int PIN_IN3 = 13;
+const int PIN_IN4 = 12;
 #endif
 
 // Servo Pin
@@ -27,11 +25,11 @@ const int PIN_ENB = 4;
 const int PIN_SER_U = 3; 
 #endif
 #if LOWER_SERVO
-const int PIN_SER_L = 15;
+const int PIN_SER_L = 2;
 #endif
 //wifi setting
-const char *ssid = "pppppp";
-const char *password = "310110199701093724";
+const char *ssid = "UR-FallTrain";
+const char *password = "falltrain";
 
 // Webserver
 WebServer server(80);
@@ -42,7 +40,6 @@ Servo lower_servo;
 int current_upper_angle = 90,current_lower_angle = 90;
 
 // Motor control
-uint8_t current_speed_l = 0,current_speed_r = 0;
 bool motor_break = 0;
 int prev_action = 1;
 
@@ -54,7 +51,7 @@ const char *html_content = R"rawliteral(
   <style>
     body { text-align: center; font-family: Arial, sans-serif; }
     table { margin: 0 auto; border-collapse: collapse; }
-    td { padding: 5px; `}
+    td { padding: 5px; }
     button { width: 160px; height: 120px; font-size: 16px; }
     .stop-btn { background-color:#ff4d4d; width:225px; margin-top:10px; } 
   </style>
@@ -64,43 +61,29 @@ const char *html_content = R"rawliteral(
     <table>
         <tr>
             <td></td>
-            <td><a href="/action?dir=1"><button>UP</button></a></td>
+            <td><button onclick="handle_dir(1)">UP</button></td>
             <td></td>
         </tr>
         <tr>
-            <td><a href="/action?dir=2"><button>LEFT</button></a></td>
-            <td></td> <td><a href="/action?dir=4"><button>RIGHT</button></a></td>
+            <td><button onclick="handle_dir(2)">LEFT</button></td>
+            <td></td> <td><button onclick="handle_dir(4)">RIGHT</button></td>
         </tr>
         <tr>
             <td></td>
-            <td><a href="/action?dir=3"><button>DOWN</button></a></td>
+            <td><button onclick="handle_dir(3)">DOWN</button></td>
             <td></td>
         </tr>
     </table>
     <div>
-        <a href="/action?dir=0"><button class="stop-btn">BREAK/RESUME</button></a>
+        <button class="stop-btn" id=stop_btn onclick="handle_dir(0)">BREAK/RESUME</button>
     </div>
     <hr>
-    <div>Current Upper Servo angle:<strong id="upper_angle_display">%CURRENT_UPPERSERVO%</strong></div>
     <div>Current Lower Servo angle:<strong id="lower_angle_display">%CURRENT_LOWERSERVO%</strong></div>
-    <div>Current L Motor Speed:<strong id="l_motor_display">%CURRENT_L_MOTOR_SPEED%</strong></div>
-    <div>Current R Motor Speed:<strong id="r_motor_display">%CURRENT_R_MOTOR_SPEED%</strong></div>
-    <div>
-        Upper Servo:
-        <input type="range" id="upper_slider" name="upper_angle" min="0" max="180" value="%CURRENT_UPPERSERVO">
-    </div>
     <div>
         Lower Servo:
         <input type="range" id="lower_slider" name="lower_angle" min="0" max="180" value="%CURRENT_LOWERSERVO">
     </div>
     <div>
-        L Motor:
-        <input type="range" id="l_motor_slider" name="l_motor" min="0" max="255" value="%CURRENT_L_MOTOR_SPEED">
-    </div>
-    <div>
-        R Motor:
-        <input type="range" id="r_motor_slider" name="r_motor" in="0" max="255" value="%CURRENT_R_MOTOR_SPEED">
-    </div>
     <script>
       // For better manual control, remove if RAM issue
       "use strict";
@@ -122,30 +105,21 @@ const char *html_content = R"rawliteral(
               }).catch(error => console.error(`Command update_servo ${name}=${value} error`,error));
           },50);
       }
-      
-      function update_motor(slider){
-        const name = slider.name;
-        const value = slider.value;
-        
-        // Update current speed display
-        document.getElementById(name+"_display").innerText = value;
-        // Handle Time out
-        clearTimeout(deboune_timer);
-        deboune_timer = setTimeout(() => {
-            fetch(`/speed?${name}=${value}`)
-            .then(res =>{
-                if(res.ok) console.log(`set ${name}=${value}`);
-                else console.error("server response error");
-            }).catch(error => console.error(`Command update_servo ${name}=${value} error`,error));
-        },50);
-      }
 
+      function handle_dir(dir){
+        clearTimeout(deboune_timer);
+        deboune_timer = setTimeout(()=>{
+            fetch(`/action?dir=${dir}`) 
+            .then(res => {
+                if(res.ok) console.log(`set dir to ${dir}`);
+                else console.error("server response error");
+            }).catch(error => console.error(`Command update dir to ${dir}`,error));
+        })
+      }
+      
       // Event Listener for servo control, remove if RAM issue
-      document.getElementById('upper_slider').addEventListener('input', (event) => { update_servo(event.target);});
       document.getElementById('lower_slider').addEventListener('input', (event) => { update_servo(event.target);});
-      // Event Listener for speed control
-      document.getElementById('l_motor_slider').addEventListener('input', (event) => { update_motor(event.target);});
-      document.getElementById('r_motor_slider').addEventListener('input', (event) => { update_motor(event.target);});
+      document.getElementById('stop_btn').addEventListener('keypress', (event) => { if(event.key == " ") handle_dir(0); });
     </script>
 </body>
 </html>
@@ -157,7 +131,6 @@ void motor_direction(int dir);
 void handle_root();
 void handle_action();
 void handle_servo();
-void handle_speed();
 
 void setup() {
   Serial.begin(115200);
@@ -182,10 +155,10 @@ void setup() {
   
   // pin setting
   #if L_MOTOR
-  pinMode(PIN_IN1, OUTPUT); pinMode(PIN_IN2, OUTPUT);  pinMode(PIN_ENA, OUTPUT);
+  pinMode(PIN_IN1, OUTPUT); pinMode(PIN_IN2, OUTPUT);  
   #endif
   #if R_MOTOR
-  pinMode(PIN_IN3, OUTPUT); pinMode(PIN_IN4, OUTPUT); pinMode(PIN_ENB,OUTPUT); 
+  pinMode(PIN_IN3, OUTPUT); pinMode(PIN_IN4, OUTPUT);  
   #endif
 
   #if UPPER_SERVO
@@ -198,23 +171,20 @@ void setup() {
   #endif
   // Stop motor
   Serial.println("Stopping motor...");
-  
   #if L_MOTOR
-  analogWrite(PIN_ENA,0); 
+  digitalWrite(PIN_IN1, LOW); digitalWrite(PIN_IN2, LOW);
   #endif
-
   #if R_MOTOR
-  analogWrite(PIN_ENB,0);
+  digitalWrite(PIN_IN3, LOW); digitalWrite(PIN_IN4, LOW);
   #endif
   
   // Status output
-  Serial.printf("Current L Motor Speed %d\nCurrent R Motor Speed %d\n",current_speed_l,current_speed_r);
-  Serial.printf("Current Lower Servo angle %d\nCurrent Upper Servo angle %d\n",current_lower_angle,current_upper_angle);
+  Serial.printf("Current Lower Servo angle %d\n",current_lower_angle);
+  // Serial.printf("Current Lower Servo angle %d\nCurrent Upper Servo angle %d\n",current_lower_angle,current_upper_angle);
   //Webserver
   server.on("/", handle_root);
   server.on("/action", handle_action);
   server.on("/servo",handle_servo);
-  server.on("/speed",handle_speed);
   server.begin(); 
   server.sendHeader("Location", "/", true);
   server.send(302, "text/plain", "");
@@ -225,6 +195,7 @@ void setup() {
 
 void loop() {
   server.handleClient();
+  delay(5);
 }
 
 
@@ -236,25 +207,16 @@ void motor_direction(int dir) {
     if(!motor_break){
       Serial.println("Motor Break!");
       #if L_MOTOR
-      analogWrite(PIN_ENA,0); 
+      digitalWrite(PIN_IN1, LOW); digitalWrite(PIN_IN2, LOW);
       #endif
       #if R_MOTOR
-      analogWrite(PIN_ENB,0);
+      digitalWrite(PIN_IN3, LOW); digitalWrite(PIN_IN4, LOW);
       #endif
       motor_break = 1;
       return;
     }else{
       Serial.println("Motor resume");
       motor_break = 0;
-
-      #if L_MOTOR
-      analogWrite(PIN_ENA,current_speed_l);
-      #endif
-      #if R_MOTOR
-      analogWrite(PIN_ENB,current_speed_r);
-      #endif
-
-      Serial.printf("Motor L Speed: %d Motor R Speed: %d\n",current_speed_l,current_speed_r);
       motor_direction(prev_action); 
       return;
     }
@@ -270,10 +232,10 @@ void motor_direction(int dir) {
   case 2:
     Serial.println("Motor move left");
     #if L_MOTOR
-    digitalWrite(PIN_IN1, LOW); digitalWrite(PIN_IN2, HIGH);
+    digitalWrite(PIN_IN1, HIGH); digitalWrite(PIN_IN2, LOW);
     #endif
     #if R_MOTOR
-    digitalWrite(PIN_IN3, HIGH); digitalWrite(PIN_IN4, LOW);
+    digitalWrite(PIN_IN3, LOW); digitalWrite(PIN_IN4, LOW);
     #endif
     break;
   case 3:
@@ -288,26 +250,15 @@ void motor_direction(int dir) {
   case 4:
     Serial.println("Motor move right");
     #if L_MOTOR
-    digitalWrite(PIN_IN1, HIGH); digitalWrite(PIN_IN2, LOW);
+    digitalWrite(PIN_IN1, LOW); digitalWrite(PIN_IN2, LOW);
     #endif
     #if R_MOTOR
-    digitalWrite(PIN_IN3, LOW); digitalWrite(PIN_IN4, HIGH);
+    digitalWrite(PIN_IN3, HIGH); digitalWrite(PIN_IN4, LOW);
     #endif
     break;
   }
   motor_break = 0;
   prev_action = dir;
-  #if L_MOTOR
-  analogWrite(PIN_ENA,current_speed_l);
-  Serial.printf("Current speed left:%d\n",current_speed_l);
-  #endif
-  #if R_MOTOR
-  analogWrite(PIN_ENB,current_speed_r);
-  Serial.printf("Current speed right:%d\n",current_speed_r);
-  #endif
-  
-
-
   /*
      * 1
     4     2
@@ -317,9 +268,7 @@ void motor_direction(int dir) {
 
 void handle_root() {
   String page = html_content;
-  page.replace("%CURRENT_L_MOTOR_SPEED%",String(current_speed_l));
-  page.replace("%CURRENT_R_MOTOR_SPEED%",String(current_speed_r));
-  page.replace("%CURRENT_UPPERSERVO%",String(current_upper_angle));
+  //page.replace("%CURRENT_UPPERSERVO%",String(current_upper_angle));
   page.replace("%CURRENT_LOWERSERVO%",String(current_lower_angle));
   server.send(200, "text/html", page); 
 }
@@ -331,29 +280,6 @@ void handle_action() {
   }
   server.sendHeader("Location", "/", true);
   server.send(302, "text/plain", "");
-}
-
-void handle_speed(){
-  #if L_MOTOR
-  if(server.hasArg("l_motor")){
-    int speed = server.arg("l_motor").toInt();
-    speed = constrain(speed,0,255);
-    analogWrite(PIN_ENA,speed);
-    current_speed_l = speed;
-    Serial.printf("Motor L Speed: %d\n",current_speed_l);
-  }
-  #endif
-  #if R_MOTOR
-  if(server.hasArg("r_motor")){
-    int speed = server.arg("r_motor").toInt();
-    speed = constrain(speed,0,255);
-    analogWrite(PIN_ENB,speed);
-    current_speed_r = speed;
-    Serial.printf("Motor R Speed: %d\n",current_speed_r);    
-  }
-  #endif
-  server.sendHeader("Location","/",true);
-  server.send(302,"text/plain","");
 }
 
 void handle_servo(){
